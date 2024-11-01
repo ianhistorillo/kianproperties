a7357ad4979f35a8434feb4570ccb93ef740a876
Christian Historillo <ianworks@Christians-MacBook-Pro.local>
2024-11-01 23:24:24 +0800

    Adding state management - react redux

---
+++ src/app/redux/reducers/counterReducer.js
@@ -0,0 +1,17 @@
+"use client";
+
+// reducers/exampleReducer.js
+const initialState = { value: 0 };
+
+const exampleReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case "INCREMENT":
+      return { ...state, value: state.value + 1 };
+    case "DECREMENT":
+      return { ...state, value: state.value - 1 };
+    default:
+      return state;
+  }
+};
+
+export default exampleReducer;
